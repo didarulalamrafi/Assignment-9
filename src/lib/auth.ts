ab19446d5e512aa650304c9aecc089ee5722282d
Didarul Alam <didarulalamrafi1312@gmail.com>
2026-06-23 12:26:59 +0600

    error

---
+++ src/lib/auth.ts
@@ -0,0 +1,12 @@
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(process.env.DATABASE_URL!);
+const db = client.db();
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db),
+  baseURL: "http://localhost:3000/",
+  emailAndPassword: { enabled: true },
+});
